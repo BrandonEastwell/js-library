ef82518db56f9667dc4a476fd91af2e5ec0ab337
Brandon Eastwell <be21160@essex.ac.uk>
2024-11-28 02:13:50 +0000

    library script functions to add books with HTML form

--- app/scripts/library.js
+++ app/scripts/library.js
@@ -1,21 +1,61 @@
 const bookLibrary = [];
+let formVisible = false;
+const table = document.getElementById("table-body");
+const form = document.getElementById("book-form");
+const addBtn = document.getElementById("add-button");
 
-function Book(title, author, pages, read) {
+addBtn.addEventListener("click", (e) => {
+    formVisible = !formVisible;
+    form.style.visibility = formVisible ? "visible" : "hidden";
+})
+
+form.addEventListener("submit", addBookToLibrary);
+
+function Book(title, author, pages, hasRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.hasRead = hasRead;
     this.info = function () {
         let string = `${this.title} by ${this.author}, ${this.pages} pages, `
-        return string += read ? 'read' : 'not read yet';
-
+        return string += hasRead ? 'read' : 'not read yet';
     }
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    bookLibrary.push(new Book(title, author, pages, read));
+function addBookToLibrary(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    bookLibrary.push(new Book(data.get("title"), data.get("author"), data.get("pages"), data.get("read")));
+    displayBooksToPage();
 }
 
 function displayBooksToPage() {
-    bookLibrary
+    let count = 0;
+    table.replaceChildren()
+    for (const book of bookLibrary) {
+        const row = document.createElement("tr");
+
+        const index = document.createElement("td");
+        index.textContent = count.toString();
+        row.appendChild(index);
+
+        const title = document.createElement("td");
+        title.textContent = book.title;
+        row.appendChild(title);
+
+        const author = document.createElement("td");
+        author.textContent = book.author;
+        row.appendChild(author);
+
+        const pages = document.createElement("td");
+        pages.textContent = book.pages;
+        row.appendChild(pages);
+
+        const hasRead = document.createElement("td");
+        hasRead.textContent = book.hasRead ? "true" : "false";
+        row.appendChild(hasRead);
+
+        table.appendChild(row);
+        count++;
+    }
 }
